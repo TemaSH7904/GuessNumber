2f47cc1bdaab1a156cfccdee29aa8b0843ac9c03
pacan <artem_shalaev00@mail.ri>
2023-10-12 19:23:42 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class Main {
-    static boolean isTrue;
+    static boolean isTrue = false;
 
     public static void main(String[] args) {
 
@@ -11,33 +11,35 @@ public class Main {
     Thread thread0 = new Thread(new Runnable() {
         @Override
         public void run() {
-        for (int i = 0; isTrue = false; i++){
-            int secondTrueRandNum = random.nextInt(0, 1_000_000_000);
-            if(trueRandNumber != secondTrueRandNum){
-                isTrue = false;
-            } else if (isTrue){
-             break;
+               for(int i = 0; !isTrue; i++){
+                   int secondRandNumber = random.nextInt(0, 1_000_000_000);
+                   if(secondRandNumber == trueRandNumber){
+                   System.out.println(secondRandNumber);
+                       isTrue = true;
+                   }
+               }
             }
-        }
-        }
-    });
+         });
     thread0.start();
 
     Thread timer = new Thread(new Runnable() {
         @Override
         public void run() {
-            while (isTrue != true){
+                for (int i = 1; i < 100; i++) {
 
-                for (int i = 1; i < 100; i++){
-                    System.out.println("Прошло " + i + " сек.");
-                    try {
-                        Thread.sleep(1000);
-                    }catch (InterruptedException ignore){
+                    if (!isTrue) {
+                        System.out.println("Прошло - "+ i + " сек.");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException ignore) {
 
+                        }
+                    } else {
+                        System.out.println("Прошло - "+ i + " сек.");
+                        break;
                     }
                 }
             }
-        }
     });
     timer.start();
 
